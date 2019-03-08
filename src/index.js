318c62ccac639f709b9743910c717f8ee4b6aabb
Harry Moreno <morenoh149@gmail.com>
2019-03-08 15:29:01 -0500

    Type Relationships
    * add Messages to project
    * user has_many messages
    * message belongs_to user

--- src/index.js
+++ src/index.js
@@ -5,30 +5,71 @@ const app = express();
 
 const schema = gql`
   type Query {
-    me: User
-    user(id: ID!): User
     users: [User!]
+    user(id: ID!): User
+    me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
 const resolvers = {
   Query: {
-    me: (parent, args, { me }) => {
-      return me;
+    users: () => {
+      return Object.values(users);
     },
     user: (parent, { id }) => {
       return users[id];
     },
-    users: () => {
-      return Object.values(users);
+    me: (parent, args, { me }) => {
+      return me;
+    },
+    messages: () => {
+      return Object.values(messages);
+    },
+    message: (parent, { id }) => {
+      return messages[id];
     },
   },
+
   User: {
+    messages: user => {
+      return Object.values(messages).filter(
+        message => message.userId === user.id,
+      );
+    },
+  },
+
+  Message: {
+    user: message => {
+      return users[message.userId];
+    },
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
   },
 };
 
@@ -36,10 +77,12 @@ let users = {
   1: {
     id: '1',
     username: 'Harry Moreno',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Ashley Adams',
+    messageIds: [2],
   },
 };
 
