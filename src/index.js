ae5ffbaa86e6895d965d208fd67ea61a9a9a45e8
Harry Moreno <morenoh149@gmail.com>
2019-03-11 16:37:52 -0400

    Connecting Resolvers and Database
    * wire up sequelize

--- src/index.js
+++ src/index.js
@@ -10,16 +10,60 @@ const app = express();
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
+  context: async () => ({
     models,
-    me: models.User.findById(1),
-  },
+    me: await models.User.findByLogin('morenoh149'),
+  }),
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
+let eraseDatabaseOnSync;
+if (process.env.NODE_ENV === 'production') {
+  eraseDatabaseOnSync = false;
+} else {
+  eraseDatabaseOnSync = true;
+}
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    seedDatabase();
+  }
+
   app.listen({ port: 9000 }, () => {
     console.log('Apollo Server on http://localhost:9000/graphql');
   });
 });
+
+const seedDatabase = async () => {
+  await models.User.create(
+    {
+      username: 'morenoh149',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
